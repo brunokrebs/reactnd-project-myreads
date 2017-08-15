039313bff5426a2e4e6be156ac30699140f1c22a
Bruno Krebs <bruno.krebs@auth0.com>
2017-08-15 09:57:25 -0300

    feat: Extract Book component from template
    Place book source code on its own component and refactor App.js to use it (fake books in the meanwhile).

---
+++ src/Book.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Book extends Component {
+    render() {
+        const bookCover = {
+            width: 128,
+            height: 192,
+            backgroundImage: 'url(' + this.props.book.imageUrl + ')'
+        };
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={bookCover} />
+                    <div className="book-shelf-changer">
+                        <select>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{this.props.book.title}</div>
+                <div className="book-authors">{this.props.book.author}</div>
+            </div>
+        )
+    }
+}
+
+export default Book;
